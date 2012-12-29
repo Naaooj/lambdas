47f73a8187b376236b0927ca7d26caace0d8a08b
Johann Bernez <joahnn.bernez@gmail.com>
2012-12-29 12:13:59 +0100

    - Refactoring base application

---
+++ src/main/java/fr/free/naoj/lambdas/ClosuresApplication.java
@@ -0,0 +1,10 @@
+package fr.free.naoj.lambdas;
+
+import fr.free.naoj.lambdas.impl.RepertoryFactoryImpl;
+
+public class ClosuresApplication {
+	
+	public static void main(String[] args) {
+		new RepertoryFactoryImpl().createAndInitARepertory();
+	}
+}
