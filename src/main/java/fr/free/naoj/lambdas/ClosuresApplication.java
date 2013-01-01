7252231fbd3925a15fbb2dd9dad57c1779d8d429
Johann Bernez <joahnn.bernez@gmail.com>
2013-01-01 18:16:35 +0100

    - Presenting a simple constructor reference

--- src/main/java/fr/free/naoj/lambdas/ClosuresApplication.java
+++ src/main/java/fr/free/naoj/lambdas/ClosuresApplication.java
@@ -6,14 +6,15 @@ import fr.free.naoj.lambdas.impl.Entry;
 
 public class ClosuresApplication {
 	
-	public static void main(String[] args) {
-		Entry entry = new Entry(0, "Demo");
+	public static void main(String[] args) {		
+		final ReferencedConstructorCaller<Entry> entryConstructor = Entry::new;
+		final ReferencedConstructorCaller<Dummy> dummyConstructor = Dummy::new;
+		final ReferencedConstructorCaller<Entry> otherEntryConstructor = (i, s) -> new Entry(i, s); 
 		
-		final ReferencedMethodCaller<Integer> getGroupIdMethod = entry::getGroupId;
-		final ReferencedMethodCaller<String> getNameMethod = entry::getName;
-		
-		printOut(() -> "Playing with a functional interface");
-		printOut(() -> getGroupIdMethod.invoke() + ":" + getNameMethod.invoke());
+		printOut(() -> "Playing with a functional interface, calling toString() on newly created object");
+		printOut(() -> entryConstructor.create(0, "Demo"));
+		printOut(() -> dummyConstructor.create(1, "DummyDemo"));
+		printOut(() -> otherEntryConstructor.create(2, "OtherDemo"));
 	}
 	
 	public static void printOut(ReferencedMethodCaller<?> caller) {
@@ -30,5 +31,34 @@ public class ClosuresApplication {
 	 */
 	private interface ReferencedMethodCaller<T> {
 		T invoke();
+	}
+	
+	/**
+	 * Functional interface representing the constructor
+	 * of type T, taking two parameters. Able to return a 
+	 * new instance of the type T
+	 * 
+	 * @author Naoj
+	 *
+	 * @param <T>
+	 */
+	private interface ReferencedConstructorCaller<T> {
+		T create(int groupId, String name);
+	}
+	
+	private static class Dummy {
+		
+		private int i;
+		private String s;
+		
+		public Dummy(int i, String s) {
+			this.i = i;
+			this.s = s;
+		}
+		
+		@Override
+		public String toString() {
+			return i + ":" + s;
+		}
 	}
 }
